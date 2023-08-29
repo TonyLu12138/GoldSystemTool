fc9be55c998c8ed3a4db76a7c84aace3bdc358ad
TonyLu12138 <138006045+TonyLu12138@users.noreply.github.com>
2023-08-29 21:26:41 +0800

    系统配置文件修改

--- configuration_system_file.py
+++ configuration_system_file.py
@@ -6,7 +6,8 @@ import subprocess
 from log_record import DebugLogger
 
 class SystemConfigModifier:
-    def __init__(self, target_mount_point, debug_enabled=False):
+    def __init__(self, target_mount_point, logical_volume, debug_enabled=False):
+        self.logical_volume = logical_volume
         self.target_mount_point = target_mount_point
         self.debug_logger = DebugLogger("SystemConfigModifier", debug_enabled)
 
@@ -16,17 +17,21 @@ class SystemConfigModifier:
         获取目标盘根目录分区的文件系统UUID
         """
         try:
-            command = f"blkid /dev/ubuntu-vg1/ubuntu-lv1"   # 修改，用全局变量
+            self.debug_logger.log("开始执行获取UUID方法")
+            command = f"blkid {self.logical_volume}"   # 修改，用全局变量
+            self.debug_logger.log(f"执行指令 {command}")
             output = subprocess.check_output(command, shell=True, text=True).strip()
             for line in output.splitlines():
                 if "UUID=" in line:
                     parts = line.split()
                     for part in parts:
                         if part.startswith("UUID="):
-                            return part.split("=")[1].strip('"')
+                            UUID = part.split("=")[1].strip('"')
+                            self.debug_logger.log(f"获取UUID {UUID}")
+                            return UUID
         except subprocess.CalledProcessError as e:
             self.debug_logger.log(f"获取UUID失败：{e}")
-        return None
+            return None
 
     # 
     def modify_fstab_file(self,UUID):
@@ -73,14 +78,21 @@ class SystemConfigModifier:
         """
         resolv_conf_path = f"{self.target_mount_point}/etc/resolv.conf"
 
+        # 删除原有的 resolv.conf 文件
+        subprocess.run(["sudo", "rm", resolv_conf_path], check=True)
+        
+        # 使用sudo命令创建resolv.conf文件
+        subprocess.run(["sudo", "touch", resolv_conf_path], check=True)
+
+        # 修改权限为666
         subprocess.run(["sudo", "chmod", "666", resolv_conf_path], check=True)
 
         # 新的 DNS 服务器地址列表
         new_dns_servers = ["nameserver 8.8.8.8", "nameserver 114.114.114.114"]
 
-        # 删除原有内容并添加新的 DNS 服务器地址
+        # 写入新的 DNS 服务器地址
         with open(resolv_conf_path, 'w') as f:
             f.writelines('\n'.join(new_dns_servers) + '\n')
 
-        # 恢复文档权限
+        # 恢复文档权限为644
         subprocess.run(["sudo", "chmod", "644", resolv_conf_path], check=True)
