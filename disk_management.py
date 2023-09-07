124ef11152f881d47319a93b37329a12923d0a94
TonyLu12138 <138006045+TonyLu12138@users.noreply.github.com>
2023-09-07 21:30:50 +0800

    磁盘管理模块

--- disk_management.py
+++ disk_management.py
@@ -1,36 +1,54 @@
 #! /usr/bin/env python3
 import os
 import re
-from log_record import TaskLogger, DebugLogger  # 导入TaskLogger类
+from base import Base
+from log_record import TaskLogger, DebugLogger
+from process import ProgressBar  # 导入TaskLogger类
 
 class TargetDiskAnalyzer:
-    def __init__(self, target_disk, debug):
+    def __init__(self, target_disk, debug_logger):
         self.target_disk = target_disk
+        self.base = Base(debug_logger)
         self.task_logger = TaskLogger("DiskManagement")  # 创建任务日志记录器
         # print(f"这是DiskManagement的debug传入值 {debug}")
-        self.debug_logger = DebugLogger("DiskManagement")
-        self.debug_logger.set_debug(debug)
+        self.debug_logger = debug_logger
+        # self.debug_logger.set_debug(debug)
 
+     # 使用Linux的'lsblk'命令来获取目标盘分区结构
     def get_partition_structure(self):
         try:
-            # 使用Linux的'lsblk'命令来获取目标盘分区结构
+            # 使用ProgressBar显示进度条
+            progress_bar = ProgressBar(1)
+
             self.debug_logger.log(f"开始执行获取目标盘分区结构方法") #debug
             command = f'lsblk -o NAME,MOUNTPOINT {self.target_disk}'
-            self.debug_logger.log(f"执行 {command} 命令。") #debug
-            partition_structure = os.popen(command).read()
-            self.debug_logger.log(f"结果输出 {partition_structure}。") #debug
+            partition_structure = self.base.com(command)
+            progress_bar.update()
+            self.task_logger.log("INFO", f"目标盘分区结构：{partition_structure}")
+            if partition_structure is not None:
+                print("目标盘分区结构：")
+                print(partition_structure)
+            else:
+                print("无法获取目标盘分区结构")
             return partition_structure
         except Exception as e:
             print(f"获取目标盘分区结构时发生错误：{e}")
             self.task_logger.log("ERROR", f"获取目标盘分区结构时发生错误：{e}")
             self.debug_logger.log(f"获取目标盘分区结构时发生错误：{e}") #debug
             return None
+        finally:
+            progress_bar.close()  # 关闭进度条
+            self.debug_logger.log("")  # 输出一个空白行
 
-    def get_root_partition(self):
+    def get_root_partition(self, partition_structure):
+
+        # 使用ProgressBar显示进度条
+        progress_bar = ProgressBar(1)
+
         try:
             # 获取目标盘分区结构
-            self.debug_logger.log(f"开始执行获取目标盘分区结构方法") #debug
-            partition_structure = self.get_partition_structure()
+            self.debug_logger.log(f"开始执行获取根目录所在分区方法") #debug
+            partition_structure = partition_structure
 
             # 处理分区结构，提取包含字母和数字的分区名
             partition_names = [re.sub(r'[^\w\d]', '', line) for line in partition_structure.splitlines() if re.search(r'\w+\d+', line)]
@@ -44,6 +62,8 @@ class TargetDiskAnalyzer:
                     print(f"根目录所在分区：{partition_name}")
                     self.task_logger.log("INFO", f"根目录所在分区：{partition_name}")
                     self.debug_logger.log(f"输出根目录所在分区 /dev/{partition_name}。") #debug
+                    progress_bar.update()
+                    print(f"目标盘根目录所在分区：/dev/{partition_name}")
                     return f"/dev/{partition_name}"
                 
 
@@ -56,6 +76,9 @@ class TargetDiskAnalyzer:
             self.task_logger.log("ERROR", f"查找根目录分区时发生错误：{e}")
             self.debug_logger.log(f"查找根目录分区时发生错误：{e}") #debug
             return None
+        finally:
+            progress_bar.close()  # 关闭进度条
+            self.debug_logger.log("")  # 输出一个空白行
 
     #尝试把它先挂载到/mnt/sdx上，
     # 然后分析里面的文件是否是根目录所包含文件。
