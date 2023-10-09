5b8acfc9485905be7a81272f2d66e8a2a03c70ae
TonyLu12138 <138006045+TonyLu12138@users.noreply.github.com>
2023-10-09 14:41:49 +0800

    进度条方法

---
+++ process.py
@@ -0,0 +1,12 @@
+from tqdm import tqdm
+
+class ProgressBar:
+    def __init__(self, max):
+        self.total_steps = max
+        self.progress_bar = tqdm(total=max, dynamic_ncols=True)
+
+    def update(self, step=1):
+        self.progress_bar.update(step)
+
+    def close(self):
+        self.progress_bar.close()
