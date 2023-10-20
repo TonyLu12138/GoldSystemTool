1295032edfab6696745deb50a2fb572da7ce3919
TonyLu12138 <138006045+TonyLu12138@users.noreply.github.com>
2023-10-20 15:43:23 +0800

    代码优化

--- disk_management.py
+++ disk_management.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 import os
 import re
+import subprocess
 from base import Base
 from process import ProgressBar  
 
@@ -9,9 +10,7 @@ class TargetDiskAnalyzer:
         self.target_disk = target_disk
         self.base = Base(task_logger, debug_logger)
         self.task_logger = task_logger  # 创建任务日志记录器
-        # print(f"这是DiskManagement的debug传入值 {debug}")
         self.debug_logger = debug_logger
-        # self.debug_logger.set_debug(debug)
 
      # 使用Linux的'lsblk'命令来获取目标盘分区结构
     def get_partition_structure(self):
@@ -51,24 +50,24 @@ class TargetDiskAnalyzer:
 
             # 处理分区结构，提取包含字母和数字的分区名
             partition_names = [re.sub(r'[^\w\d]', '', line) for line in partition_structure.splitlines() if re.search(r'\w+\d+', line)]
-            print('partition_names' + str(partition_names))   #删除
             self.task_logger.log("DEBUG", 'partition_names' + str(partition_names))
             self.debug_logger.log(f"提取分区结构信息 {partition_names}。") #debug
             
             # 逐个分区进行判断
             for partition_name in partition_names:
-                if partition_name == 'sdd2':
-                    print(f"根目录所在分区：{partition_name}")
-                    self.task_logger.log("INFO", f"根目录所在分区：{partition_name}")
-                    self.debug_logger.log(f"输出根目录所在分区 /dev/{partition_name}。") #debug
-                    progress_bar.update()
-                    print(f"目标盘根目录所在分区：/dev/{partition_name}")
-                    return f"/dev/{partition_name}"
+                partition_path = f'/dev/{partition_name}'
+                if os.path.exists(partition_path):
+                    # 使用 subprocess 运行 blkid 命令来获取分区信息
+                    blkid_info = subprocess.check_output(["blkid", partition_path], universal_newlines=True)
+                    self.debug_logger.log(f"运行 blkid 命令来获取{partition_path}分区信息: {blkid_info}。") #debug
+                    # 查找文件系统类型
+                    if "ext4" in blkid_info:  # 用实际的文件系统类型替代 "ext4"
+                        print(f"根目录所在分区：{partition_name}")
+                        return partition_name
                 
-
-            print("未找到sdd2 的根分区")
-            self.task_logger.log("WARNING", "未找到sdd2的根分区")
-            self.debug_logger.log("未找到sdd2的根分区") #debug
+            print("未找到目标磁盘的根分区")
+            self.task_logger.log("WARNING", "未找到目标磁盘的根分区")
+            self.debug_logger.log("未找到目标磁盘的根分区") #debug
             return None
         except Exception as e:
             print(f"查找根目录分区时发生错误：{e}")
